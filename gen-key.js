f6e9e622ee85bdcdbf70bacdec8567e77fcceac1
Markusha <LegoAnimation2022@gmail.com>
2025-06-10 16:20:41 +0300

    Create gen-key.js

---
+++ gen-key.js
@@ -0,0 +1,6 @@
+import webpush from 'web-push';
+
+const vapidKeys = webpush.generateVAPIDKeys();
+
+console.log('PUBLIC_KEY=' + vapidKeys.publicKey);
+console.log('PRIVATE_KEY=' + vapidKeys.privateKey);
